f42e010c4379827c3fe08eb2fdf1d4df6721fa11
astroerik <dani008molina@gmail.com>
2023-01-04 16:55:10 -0500

    Fix Content in App

--- frontend_mentorias/src/App.js
+++ frontend_mentorias/src/App.js
@@ -2,23 +2,20 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
 import Login from "./pages/Login";
 import Register from './pages/Register';
-import Contents from './pages/Contents';
-import AddEntrepreneur from './pages/Entrepreneurs/AddEntrepreneur';
-import Entrepreneurs from './pages/Entrepreneurs/Entrepreneurs';
 import Managers from './pages/Managers/Managers';
 import AddManager from './pages/Managers/AddManager';
 import Products from './pages/Products/Products';
 import AddProduct from './pages/Products/AddProduct';
 import Mentorships from './pages/Mentorships/Mentorships';
 import AddMentorship from './pages/Mentorships/AddMentorship';
-
-
+import Contents from './pages/Contents/Contents';
+import AddContents from './pages/Contents/AddContents';
+import AddEntrepreneur from './pages/Entrepreneurs/AddEntrepreneur';
+import Entrepreneurs from './pages/Entrepreneurs/Entrepreneurs';
 
 
 function App() {
   return (
-    
-  
         <Routes>
           <Route exact path='/' element={<Dashboard/>}/>
           <Route exact path='/login' element={<Login/>}/>
@@ -30,6 +27,7 @@ function App() {
           <Route exact path='/mentorships' element={<Mentorships/>}/>
           <Route exact path='/mentorships/add' element={<AddMentorship/>}/>
           <Route exact path='/contents' element={<Contents/>}/>
+          <Route exact path='/contents/add' element={<AddContents/>}/>
           <Route exact path='/entrepreneurs' element={<Entrepreneurs/>}/>
           <Route exact path='/entrepreneurs/add' element={<AddEntrepreneur/>}/>
         </Routes>
@@ -37,3 +35,4 @@ function App() {
 }
 
 export default App;
+
