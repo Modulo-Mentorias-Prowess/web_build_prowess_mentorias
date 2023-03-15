6707560b66262e38f6cd2420f084b908bcfe5f8f
CarielR <arielcastrooo7890@hotmail.com>
2023-03-15 11:39:59 -0500

    correccion carusel

--- frontend_mentorias/src/pages/Dashboard.jsx
+++ frontend_mentorias/src/pages/Dashboard.jsx
@@ -10,33 +10,33 @@ const Dashboard = () => {
   const images = [
 		{
 			id: '1',
-			title: 'Lorem Ipsun',
+			title: '1',
 			image:
 				'https://cdn.pixabay.com/photo/2020/11/09/15/12/trail-5726987_960_720.jpg',
 		},
 		{
 			id: '2',
-			title: 'Lorem Ipsun.',
+			title: '2',
 			image:
 				'https://cdn.pixabay.com/photo/2011/09/27/18/52/bird-9950_960_720.jpg',
 		},
 		{
 			id: '3',
-			title: 'Lorem Ipsun.',
+			title: '3.',
 			image:
 				'https://cdn.pixabay.com/photo/2017/12/27/14/02/friends-3042751_960_720.jpg',
 		},
 		{
 			id: '4',
-			title: 'Lorem Ipsun.',
+			title: '4.',
 			image:
 				'https://cdn.pixabay.com/photo/2016/11/21/17/44/arches-national-park-1846759_960_720.jpg',
 		},
 		{
 			id: '5',
-			title: 'Lorem Ipsun.',
+			title: '5.',
 			image:
-				'https://cdn.pixabay.com/photo/2018/10/26/22/55/harley-davidson-3775527_950_720.jpg',
+				'https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_1280.jpg',
 		},
 	]
 
@@ -50,7 +50,12 @@ const Dashboard = () => {
     <div className="bg-main-prowess min-h-screen">
       <Navbar />
       {/* Dashboard */}
-      <Slider images={images} />
+	  <div  >
+      	<Slider images={images}  />
+		  
+	  
+	  </div>
+	  <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatem similique, tenetur, laudantium maxime fuga eius placeat, atque quod quasi aspernatur? Sit impedit explicabo provident asperiores odio sint esse id.</h1>
       {/* Final */}
     </div>
   );
