20fd643e7ed6cb4b224781f5ab08b4e3d5ec1e5b
CarielR <arielcastrooo7890@hotmail.com>
2023-03-29 16:13:15 -0500

    v2

--- frontend_mentorias/src/pages/Mentorships/AddMentorship.jsx
+++ frontend_mentorias/src/pages/Mentorships/AddMentorship.jsx
@@ -376,7 +376,7 @@ const AddMentorship = () => {
               <input
                 //className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 
-                className={errors.names ? errorStyle : normalStyle}
+                className={errors.date_mentorship ? errorStyle : normalStyle}
                 name="date_mentorship"
                 onChange={handleChange}
                 onBlur={handleLostFocus}
