80388a61662d5f72377b276017ee15cbf041d22e
Mateo M <69735785+Fm4mdR03@users.noreply.github.com>
2023-04-03 10:52:15 -0500

    Cambios Botón de Whatsapp
    Cambio de logo, posición y link de botón de whatsapp

--- frontend_mentorias/src/pages/CHAT/wpp.jsx
+++ frontend_mentorias/src/pages/CHAT/wpp.jsx
@@ -3,16 +3,16 @@ import './wpp.css';
 import whatsappIcon from './whatsapp32.svg';
 
 function WhatsAppButton() {
-    return (
-      <a
-        href="https://wa.me/"
-        className="whatsapp-button"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={whatsappIcon} alt="WhatsApp" />
-      </a>
-    );
-  }
+  return (
+    <a
+      href="https://api.whatsapp.com/send?phone=593991234567&text=MENSAJE%20DE%20PRUEBA"
+      className="whatsapp-button"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={whatsappIcon} alt="WhatsApp" />
+    </a>
+  );
+}
 
 export default WhatsAppButton;
