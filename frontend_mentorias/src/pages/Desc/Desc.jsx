b6da3068977e7a90869bfb25ee74bc317cdeb30c
Stephy3 <131786609+Stephy3@users.noreply.github.com>
2023-06-08 16:40:55 -0500

    infe

--- frontend_mentorias/src/pages/Desc/Desc.jsx
+++ frontend_mentorias/src/pages/Desc/Desc.jsx
@@ -8,7 +8,7 @@ function DescriptionSection() {
         <div className="description">
           <h2>PROWESS</h2>
           <p>
-            Lorem ipsum, s, aut amet aspernatur officia illum pariatur perferendis sequi blanditiis ullam officiis quam nemo doloremque! Corporis blanditiis magni sunt inventore magnam reiciendis vitae fuga voluptatum ratione consequatur? Est, molestias.
+            Lorem ipsum, s, aut amet aspernatur officia illum pariatur perferendis sequi blanditiis ullam officiis quam nemo doloremque! Corporis blanditiis magni sunt inventore magnam reiciendis vitae fuga voluptatum ratione consequatur Est, molestias.
           </p>
           <p> amet consectetur adipisicing elit. Non alias aliquid quibusdam sit, facere quas corrupti cupiditate ullam optio dignissimos voluptate esse distinctio exercitationem quae saepe doloremque soluta eos minus.</p>
 
