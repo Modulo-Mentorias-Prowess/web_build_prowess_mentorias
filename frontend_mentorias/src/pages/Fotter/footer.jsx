71745bccebd5254f9df13dcd7b8b2df3cfddb7f7
Joel976 <ajcuenca4@espe.edu.ec>
2023-08-02 17:48:13 -0500

    Correción

--- frontend_mentorias/src/pages/Fotter/footer.jsx
+++ frontend_mentorias/src/pages/Fotter/footer.jsx
@@ -4,17 +4,19 @@ import instagramIcon from './instagram32.svg';
 import tiktokIcon from './tiktok32.svg';
 
 function Footer() {
-  const iconRef = useRef(null);
+  const instagramIconRef = useRef(null); // Referencia para el ícono de Instagram
+  const tiktokIconRef = useRef(null); // Referencia para el ícono de TikTok
 
   const handleInstagramIconClick = (event) => {
-    // Comprobamos si el evento se originó desde el elemento del icono
-    if (event.target === iconRef.current) {
+    // Comprobamos si el evento se originó desde el elemento del icono de Instagram
+    if (event.target === instagramIconRef.current) {
       window.open('https://instagram.com/prowess_ec_contenido?igshid=NTc4MTIwNjQ2YQ==', '_blank');
     }
   };
 
   const handleTikTokIconClick = (event) => {
-    if (event.target === iconRef.current) {
+    // Comprobamos si el evento se originó desde el elemento del icono de TikTok
+    if (event.target === tiktokIconRef.current) {
       window.open('https://www.tiktok.com/@prowessecuador?_t=8eUBct5E5nK&_r=1', '_blank');
     }
   };
@@ -24,7 +26,7 @@ function Footer() {
       <p className="rights">&copy; 2023 - Todos los derechos reservados</p>
       <div className="icon-container">
         <img
-          ref={iconRef} 
+          ref={instagramIconRef} // Utilizar la referencia de Instagram aquí
           src={instagramIcon}
           alt="Instagram"
           className="icon instagram-icon"
@@ -32,7 +34,7 @@ function Footer() {
           onClick={handleInstagramIconClick}
         />
         <img
-          ref={iconRef} 
+          ref={tiktokIconRef} // Utilizar la referencia de TikTok aquí
           src={tiktokIcon}
           alt="TikTok"
           className="icon tiktok-icon"
