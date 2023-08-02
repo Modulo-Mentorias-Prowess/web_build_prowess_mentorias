91ccbe96742626c75eea42f87c232aed6e955550
Joel976 <ajcuenca4@espe.edu.ec>
2023-08-02 17:33:43 -0500

    Tiktok

--- frontend_mentorias/src/pages/Fotter/footer.jsx
+++ frontend_mentorias/src/pages/Fotter/footer.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import './footer.css';
 import instagramIcon from './instagram32.svg';
+import tiktokIcon from './tiktok32.svg';
 
 function Footer() {
   const iconRef = useRef(null);
@@ -12,16 +13,31 @@ function Footer() {
     }
   };
 
+  const handleTikTokIconClick = (event) => {
+    if (event.target === iconRef.current) {
+      window.open('https://www.tiktok.com/@prowessecuador?_t=8eUBct5E5nK&_r=1', '_blank');
+    }
+  };
+
   return (
     <footer className="footer">
       <p className="rights">&copy; 2023 - Todos los derechos reservados</p>
-      <div className="icon-container" onClick={handleInstagramIconClick}>
+      <div className="icon-container">
         <img
           ref={iconRef} 
           src={instagramIcon}
           alt="Instagram"
           className="icon instagram-icon"
           id="instagramIcon"
+          onClick={handleInstagramIconClick}
+        />
+        <img
+          ref={iconRef} 
+          src={tiktokIcon}
+          alt="TikTok"
+          className="icon tiktok-icon"
+          id="tiktokIcon"
+          onClick={handleTikTokIconClick}
         />
       </div>
     </footer>
