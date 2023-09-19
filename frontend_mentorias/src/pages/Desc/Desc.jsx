2e90be7f993d408683712da57294f89a24d2f3e3
FernandoQ25 <romelquinapallo10@gmail.com>
2023-09-19 11:29:18 -0500

    He corregido las faltas ortográficas y gramaticales

--- frontend_mentorias/src/pages/Desc/Desc.jsx
+++ frontend_mentorias/src/pages/Desc/Desc.jsx
@@ -3,33 +3,26 @@ import "./DescriptionSection.css";
 import { NavLink } from "react-router-dom";
 
 function DescriptionSection() {
-    return (
-      <div className="description-container">
-        <div className="description">
-          <h2>PROWESS</h2>
-          <p>
-              Asistencia técnica para el fortalecimiento de emprendimientos de personas refugiadas, solicitantes de asilo y personas de interés de ACNUR en Pichincha y Santo Domingo de los Tsáchilas ACNUR-HIAS-ESPE- (para población en movilidad humana).
-          </p>
-          <h2>MENTORIAS</h2>
-          <p> 
-              Realizar un seguimiento continuo de los emprendimientos para tener un control adecuado de los mismos por medio de mentorías y verificar si están conformes con el trabajo realizado por parte de Prowess Ec. teniendo una idea de su crecimiento. 
-          </p>
-          <NavLink
-                to="/Mentorships"
-                className="botonDesc"
-              >
-                Ir a Mentorías
-              </NavLink>
-
-         </div>
-        <div className="image-container">
-          <img
-            src="/2.png"
-            alt="IMG"
-          />
-        </div>
+  return (
+    <div className="description-container">
+      <div className="description">
+        <h2>PROWESS</h2>
+        <p>
+          Asistencia técnica para el fortalecimiento de emprendimientos de personas refugiadas, solicitantes de asilo y personas de interés de ACNUR en Pichincha y Santo Domingo de los Tsáchilas. ACNUR-HIAS-ESPE (para población en movilidad humana).
+        </p>
+        <h2>MENTORÍAS</h2>
+        <p>
+          Realizar un seguimiento continuo de los emprendimientos para tener un control adecuado de los mismos por medio de mentorías y verificar si están conformes con el trabajo realizado por parte de Prowess Ec., teniendo una idea de su crecimiento.
+        </p>
+        <NavLink to="/Mentorships" className="botonDesc">
+          Ir a Mentorías
+        </NavLink>
       </div>
-    );
-  }
-  
-  export default DescriptionSection;
+      <div className="image-container">
+        <img src="/2.png" alt="IMG" />
+      </div>
+    </div>
+  );
+}
+
+export default DescriptionSection;
